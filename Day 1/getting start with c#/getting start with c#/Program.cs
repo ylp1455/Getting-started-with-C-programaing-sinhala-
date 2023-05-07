f7bc2f90ab94d468f6eec9dde2abcd03f0d3c0b5
Yasiru Perera <yasiruperera682@gmail.com>
2023-05-07 23:57:11 +0530

    Day 01 commit

---
+++ Day 1/getting start with c#/getting start with c#/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace getting_start_with_c_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // write a C# program to print your name
+
+            Console.WriteLine("C# sinhala tutorial");
+            
+
+            /// write a C# program to draw a pyramid using stars
+            Console.WriteLine("     *  ");
+            Console.WriteLine("  *    * ");
+            Console.WriteLine("*    *   *");
+            Console.WriteLine("");
+
+            /// write a C# program to print following pattern
+            /// *
+            /// * *
+            /// * * *
+            /// * * * *
+            /// * * * * *
+
+
+            Console.WriteLine("*");
+            Console.WriteLine("* *");
+            Console.WriteLine("* * *");
+            Console.WriteLine("* * * *");
+            Console.WriteLine("* * * * *");
+            Console.WriteLine("");
+
+            /// write a C# program to print following pattern
+            /// 1
+            /// 1 2
+            /// 1 2 3
+            /// 1 2 3 4
+            /// 1 2 3 4 5
+
+            Console.WriteLine("1");
+            Console.WriteLine("1 2");
+            Console.WriteLine("1 2 3");
+            Console.WriteLine("1 2 3 4");
+            Console.WriteLine("1 2 3 4 5");
+            Console.WriteLine("");
+
+            Console.ReadLine();
+
+        }
+    }
+}
