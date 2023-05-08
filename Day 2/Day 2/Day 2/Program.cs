0dafcee51962bdc7446b8b31dd4d733b925513db
Yasiru Perera <yasiruperera682@gmail.com>
2023-05-08 15:25:47 +0530

    Day 2

---
+++ Day 2/Day 2/Day 2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //3.  write a C# program to calculate the gross pay of an employee
+            // gross pay = hourly rate  * hours worked
+
+            double hours;
+            double rate;
+            double grossPay;
+
+            Console.WriteLine("Enter the hours wroked");
+            hours = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the hourly rate");
+            rate = double.Parse(Console.ReadLine());
+
+            grossPay = hours * rate;
+
+            Console.WriteLine("Gross pay is " + grossPay);
+
+
+            Console.ReadLine();
+        }
+    }
+}
