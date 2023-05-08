9808b8d488ab5e809e4c054831c6625d86730751
Yasiru Perera <yasiruperera682@gmail.com>
2023-05-08 21:49:06 +0530

    Day 02

--- Day 2/Day 2/Day 2/Program.cs
+++ Day 2/Day 2/Day 2/Program.cs
@@ -83,6 +83,8 @@ namespace Day_2
 
             */
 
+            /*
+
             /// Make a variable called marks 
             /// Enter soe value to the marks
             /// Accroding to vaue ogive the results
@@ -92,13 +94,120 @@ namespace Day_2
 
             double marks;
 
-            Console.WriteLine("Eneter the makrs of student");
+            Console.WriteLine("Enter the makrs of student");
             
             marks = double.Parse(Console.ReadLine());
 
-            if (marks < 50) { Console.WriteLine("Bad"); }
-            else if (marks == 50) { Console.WriteLine("Ok"); }
-            else { Console.WriteLine("Excelent"); }
+            if (marks < 50)
+            {
+                Console.WriteLine("Bad");
+            }
+            else if (marks == 50)
+            {
+                Console.WriteLine("Ok");
+            }
+            else
+            {
+                Console.WriteLine("Excellent");
+            }
+
+            */
+
+
+            /*
+             Make a code to display the given Nuber 1 to 10
+            EX:
+                if user enterd 1 as input , output "You enterd One"
+                if user enterd 2 as input , output "You enterd Two"
+                -------------------------------------------------
+                -------------------------------------------------
+                if user enterd 10 as input , output "You enterd Ten"
+                
+                
+
+            
+            int num;
+            Console.WriteLine("Enter a number one to ten");
+            num = int.Parse(Console.ReadLine());
+
+            switch (num) 
+            {
+
+                case 0:
+                    Console.WriteLine("You enterd zero");
+                    break;
+
+                case 1:
+                    Console.WriteLine("You enterd One");
+                    break;
+
+                case 2:
+                    Console.WriteLine("You enterd Two");
+                    break;
+
+                case 3:
+                    Console.WriteLine("You enterd Three");
+                    break;
+                case 4:
+                    Console.WriteLine("You enterd Four");
+                    break;
+                case 5:
+                    Console.WriteLine("You enterd Five ");
+                    break;
+                case 6:
+                    Console.WriteLine("You enterd Six");
+                    break;
+                case 7:
+                    Console.WriteLine("You enterd Seven");
+                    break;
+                case 8:
+                    Console.WriteLine("You enterd Eight");
+                    break;
+                case 9:
+                    Console.WriteLine("You enterd Nine");
+                    break;
+                case 10:
+                    Console.WriteLine("You enterd ten");
+                    break;
+               
+
+            }
+            */
+
+            /*
+             
+            loops 
+
+            C# has 3 kind of loops 
+                1 while loop
+                2 for loop
+                3 do while loop
+
+             
+
+
+            
+            while (num <= 1000) 
+            {
+                Console.WriteLine(num);
+                num++;
+            }
+
+            
+            int num = 0;
+            do
+            {
+
+                Console.WriteLine(num);
+                num++;
+
+            } while (num <= 1000);
+            */
+
+            
+
+            for (int i = 0; i < 1001; i++)
+            { Console.WriteLine(i); }
 
             Console.ReadLine();
         }
