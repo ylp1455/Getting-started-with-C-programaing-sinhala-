85f7527918b9e41615e6a4101ea2df1e6f9c3234
Yasiru Perera <yasiruperera682@gmail.com>
2023-05-08 23:56:20 +0530

    Day 02

---
+++ Day 2/Day 2 question 02/Day 2 question 02/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlTypes;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Day_2_question_02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double num1 = double.Parse(textBox1.Text);
+            double num2 = double.Parse(textBox2.Text);
+
+            double add = num1 + num2;
+            double sub = num1 - num2;
+            double mul = num1 * num2;
+            double div = num1 / num2;
+
+            string addString = add.ToString();
+            string subString = sub.ToString();
+            string mulString = mul.ToString();
+            string divString = div.ToString();
+
+            textBox3.Text = addString;
+            textBox4.Text = subString;
+            textBox5.Text = mulString;
+            textBox6.Text = divString;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            textBox6.Text = "";
+        }
+    }
+}
