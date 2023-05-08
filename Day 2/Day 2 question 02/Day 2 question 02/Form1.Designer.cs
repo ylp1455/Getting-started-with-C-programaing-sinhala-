40e7dcadfde16f3d52f43031e7b8fdb461241963
Yasiru Perera <yasiruperera682@gmail.com>
2023-05-09 02:34:54 +0530

    Day 02

--- Day 2/Day 2 question 02/Day 2 question 02/Form1.Designer.cs
+++ Day 2/Day 2 question 02/Day 2 question 02/Form1.Designer.cs
@@ -173,7 +173,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(171, 38);
             this.button1.TabIndex = 3;
-            this.button1.Text = "Calculate";
+            this.button1.Text = "Answer";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
